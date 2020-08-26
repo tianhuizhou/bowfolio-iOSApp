f055859fb5745abdd0eec5a7231771913dd1640a
tianhuizhou <tianhui@hawaii.edu>
2020-08-26 02:59:14 -1000

    successfully implement the Firebase
    I am able to log in with initial account to access the second page.
    May need to re-design the page model and UI.

---
+++ BowFolios/BowFolios/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  BowFolios
+//
+//  Created by Tianhui Zhou on 8/26/20.
+//  Copyright © 2020 Tianhui Zhou. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseCore
+
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BowFolios/BowFolios/ViewController.swift
+++ BowFolios/BowFolios/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseCore
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        let email = emailField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        //sign in the user
+        Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+            if error == nil {
+                if user != nil {
+                   self.performSegue(withIdentifier: "homeSegue", sender: self)
+                }
+            }
+        })
+    }
+    
 }
 
